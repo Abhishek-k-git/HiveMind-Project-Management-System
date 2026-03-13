e0813934b7694a4d4b3bf97ba7b71f91443e63c5
Abhishek kumar <abhishekpatel66029@gmail.com>
2026-03-13 14:49:59 +0530

    implement task module

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, {Request, Response, NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import session from "cookie-session";
 import config from "./configs/env-config";
@@ -15,35 +15,36 @@ import userRoutes from "./routes/user-route";
 import workspaceRoutes from "./routes/workspace-route";
 import memberRoutes from "./routes/member-route";
 import projectRoutes from "./routes/project-route";
+import taskRoutes from "./routes/task-route";
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(
-   cors({
-      origin: config.FRONTEND_URL,
-      credentials: true,
-   })
-)
+  cors({
+    origin: config.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(
-   session({
-      name: "session",
-      keys: [config.SESSION_SECRET],
-      maxAge: Number(config.SESSION_EXPIRES_IN),
-      secure: config.NODE_ENV === "production",
-      httpOnly: true,
-      sameSite: "lax",
-   })
-)
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: Number(config.SESSION_EXPIRES_IN),
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  }),
+);
 app.use((req: Request, res: Response, next: NextFunction) => {
-   if (req.session && !req.session.regenerate) {
-      req.session.regenerate = (cb: any) => cb();
-   }
-   if (req.session && !req.session.save) {
-      req.session.save = (cb: any) => cb();
-   }
-   next();
+  if (req.session && !req.session.regenerate) {
+    req.session.regenerate = (cb: any) => cb();
+  }
+  if (req.session && !req.session.save) {
+    req.session.save = (cb: any) => cb();
+  }
+  next();
 });
 app.use(passport.initialize());
 app.use(passport.session());
@@ -53,15 +54,18 @@ app.use(passport.session());
 //       res.status(200).json({message: "Hello from the server!"});
 //    }
 // ));
-app.use('/api/auth', authRoutes);
-app.use('/api/users', isAuthenticated, userRoutes);
-app.use('/api/workspaces', isAuthenticated, workspaceRoutes);
-app.use('/api/members', isAuthenticated, memberRoutes);
-app.use('/api/projects', isAuthenticated, projectRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", isAuthenticated, userRoutes);
+app.use("/api/workspaces", isAuthenticated, workspaceRoutes);
+app.use("/api/members", isAuthenticated, memberRoutes);
+app.use("/api/projects", isAuthenticated, projectRoutes);
+app.use("/api/tasks", isAuthenticated, taskRoutes);
 
 app.use(errHandlerMiddleware);
 
 app.listen(config.PORT, async () => {
-   console.log(`Server is listening on port ${config.PORT} in ${config.NODE_ENV} mode`);
-   await connectMongo();
+  console.log(
+    `Server is listening on port ${config.PORT} in ${config.NODE_ENV} mode`,
+  );
+  await connectMongo();
 });
