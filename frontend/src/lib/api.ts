59a7cc762906eb83663fddb207610ac08fb78563
Abhishek kumar <abhishekpatel66029@gmail.com>
2026-03-17 18:44:56 +0530

    add auth

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,17 +1,28 @@
 import API from "./axios-client";
-import { CurrentUserResponseType } from "@/types/api.type";
+import {
+    CurrentUserResponseType,
+    loginType,
+    LoginResponseType,
+    registerType,
+} from "@/types/api.type";
 
-export const loginMutationFn = async () => {};
+export const loginMutationFn = async (
+    data: loginType,
+): Promise<LoginResponseType> => {
+    const response = await API.post("/auth/login", data);
+    return response.data;
+};
 
-export const registerMutationFn = async () => {};
+export const registerMutationFn = async (data: registerType) =>
+    await API.post("/auth/register", data);
 
-export const logoutMutationFn = async () => {};
+export const logoutMutationFn = async () => await API.post("/auth/logout");
 
 export const getCurrentUserQueryFn =
-  async (): Promise<CurrentUserResponseType> => {
-    const response = await API.get(`/user/current`);
-    return response.data;
-  };
+    async (): Promise<CurrentUserResponseType> => {
+        const response = await API.get(`/user/current`);
+        return response.data;
+    };
 
 //********* WORKSPACE ****************
 //************* */
