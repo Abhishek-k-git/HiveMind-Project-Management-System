a9bb9059b67f945f8665ad7cc56bbd75f7aefd2a
Abhishek kumar <abhishekpatel66029@gmail.com>
2026-03-18 22:58:24 +0530

    implement workspace

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -6,6 +6,12 @@ import {
     registerType,
     AllWorkspaceResponseType,
     WorkspaceByIdResponseType,
+    CreateWorkspaceType,
+    CreateWorkspaceResponseType,
+    EditWorkspaceType,
+    AnalyticsResponseType,
+    ChangeWorkspaceMemberRoleType,
+    AllMembersInWorkspaceResponseType,
 } from "@/types/api.type";
 
 export const loginMutationFn = async (
@@ -42,20 +48,67 @@ export const getWorkspaceByIdQueryFn = async (
     return response.data;
 };
 
-export const createWorkspaceMutationFn = async () => {};
+export const createWorkspaceMutationFn = async (
+    data: CreateWorkspaceType,
+): Promise<CreateWorkspaceResponseType> => {
+    const response = await API.post(`/workspaces/create/new`, data);
+    return response.data;
+};
 
-export const editWorkspaceMutationFn = async () => {};
+export const editWorkspaceMutationFn = async ({
+    workspaceId,
+    data,
+}: EditWorkspaceType) => {
+    const response = await API.put(`/workspaces/update/${workspaceId}`, data);
+    return response.data;
+};
 
+export const getWorkspaceAnalyticsQueryFn = async (
+    workspaceId: string,
+): Promise<AnalyticsResponseType> => {
+    const response = await API.get(`/workspaces/analytics/${workspaceId}`);
+    return response.data;
+};
 
-export const getWorkspaceAnalyticsQueryFn = async () => {};
+export const changeWorkspaceMemberRoleMutationFn = async ({
+    workspaceId,
+    data,
+}: ChangeWorkspaceMemberRoleType) => {
+    const response = await API.put(
+        `/workspaces/change/member/role/${workspaceId}`,
+        data,
+    );
+    return response.data;
+};
 
-export const changeWorkspaceMemberRoleMutationFn = async () => {};
+export const deleteWorkspaceMutationFn = async (
+    workspaceId: string,
+): Promise<{
+    message: string;
+    currentWorkspace: string;
+}> => {
+    const response = await API.delete(`/workspaces/delete/${workspaceId}`);
+    return response.data;
+};
 
-export const deleteWorkspaceMutationFn = async () => {};
+export const getMembersInWorkspaceQueryFn = async (
+    workspaceId: string,
+): Promise<AllMembersInWorkspaceResponseType> => {
+    const response = await API.get(`/workspaces/members/${workspaceId}`);
+    return response.data;
+};
 
 //*******MEMBER ****************
 
-export const invitedUserJoinWorkspaceMutationFn = async () => {};
+export const invitedUserJoinWorkspaceMutationFn = async (
+    inviteCode: string,
+): Promise<{
+    message: string;
+    workspaceId: string;
+}> => {
+    const response = await API.post(`/members/workspace/${inviteCode}/join`);
+    return response.data;
+};
 
 //********* */
 //********* PROJECTS
